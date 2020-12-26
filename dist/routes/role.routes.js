b320f5ca37da9d017bad83e9bfaeab874ba2903f
Tery <terrychinen55@gmail.com>
2020-12-26 14:14:31 -0500

    Agregando documentacion en category y role (Solo de crear category y role, y de obtener todos los categories y roles)

--- dist/routes/role.routes.js
+++ dist/routes/role.routes.js
@@ -3,6 +3,163 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const role_controller_1 = require("../controllers/role.controller");
 const router = express_1.Router();
+/**
+ * @api {get} /role?offset=0&state=1 Obtener todos los roles
+ * @apiName ObtenerRoles
+ * @apiGroup Rol
+ *
+ * @apiHeaderExample {json} Header-Example:
+ *    {
+ *       "version": "xxxxx",
+ *       "token": "xxxx.xxxx.xxxx"
+ *    }
+ *
+ *
+ * @apiParam {Number} offset Numero de indice
+ * @apiParam {Number} state El estado del rol (0, 1)
+ *
+ * @apiSuccess {bool} ok Si la peticion ha sido exitosa o no
+ * @apiSuccess {String} message Mensaje del servidor
+ * @apiSuccess {String} result La lista de roles
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+            "ok": true,
+            "message": "Query successful",
+            "result": [
+                {
+                    "role_id": 1,
+                    "role_name": "Administrador",
+                    "state": 1
+                },
+                {
+                    "role_id": 2,
+                    "role_name": "Cajero",
+                    "state": 1
+                }
+            ]
+ *     }
+ *
+ *
+ *
+ * @apiError UpdateApp Necesita actualizar la aplicación
+ * @apiErrorExample AppVersion: 406
+ *     HTTP/1.1 406 Version error
+ *     {
+            "ok": false,
+            "message": "Actualiza la apliación",
+ *     }
+ *
+ *
+ *
+ *
+ * @apiError OffsetOrStateNotFound El 'offset' o 'state' no ha sido encontrado
+ * @apiErrorExample OffsetOrStateNotFound: 405
+ *     HTTP/1.1 405 Not Found
+ *     {
+            "ok": false,
+            "message": "La variable 'offset' y 'state' son obligatorio!"
+ *     }
+ *
+ *
+ *
+ * @apiError JWTNotFound El 'token' no ha sido encontrado
+ * @apiErrorExample JWTNotFound: 401
+ *     HTTP/1.1 401 JWTNotFound
+ *     {
+            "ok": false,
+            "name": "TokenExpiredError",
+            "message": "jwt expired",
+            "expiredAt": "2020-12-26T16:01:48.000Z"
+ *     }
+ *
+ *
+ *
+ *
+ * @apiError ServeError Error del servidor
+ * @apiErrorExample ServeError: 500
+ *     HTTP/1.1 500 Internal Server Error
+ *     {
+            "ok": false,
+            "message": "Mensaje de error del servidor"
+ *     }
+ *
+ *
+ */
+/**
+ * @api {post} /role Crear rol
+ * @apiName CrearRol
+ * @apiGroup Rol
+ *
+ * @apiHeaderExample {json} Header-Example:
+ *    {
+ *       "version": "xxxxx",
+ *       "token": "xxxx.xxxx.xxxx"
+ *    }
+ *
+ *
+ * @apiParam {String} rol_name El nombre del rol
+ * @apiParam {Number} state El estado del rol (0, 1)
+ *
+ * @apiSuccess {bool} ok Si la peticion ha sido exitosa o no
+ * @apiSuccess {String} message Mensaje del servidor
+ *
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+           "ok": true,
+           "message": "Rol creado correctamente"
+ *     }
+ *
+ *
+ *
+ * @apiError RoleExists Ya existe el rol
+ * @apiErrorExample RoleExists: 400
+ *     HTTP/1.1 406 Role exists
+ *     {
+           "ok": false,
+           "message": "El rol ya existe!",
+ *     }
+ *
+ *
+ *
+ * @apiError JWTNotFound El 'token' no ha sido encontrado
+ * @apiErrorExample JWTNotFound: 401
+ *     HTTP/1.1 401 JWTNotFound
+ *     {
+           "ok": false,
+           "name": "TokenExpiredError",
+           "message": "jwt expired",
+           "expiredAt": "2020-12-26T16:01:48.000Z"
+ *     }
+ *
+ *
+ *
+ *
+ * @apiError UpdateApp Necesita actualizar la aplicación
+ *
+ * @apiErrorExample UpdateApp: 406
+ *     HTTP/1.1 406 Need to update
+ *     {
+           "ok": false,
+           "message": "Actualiza la apliación",
+ *     }
+ *
+ *
+ *
+ *
+ * @apiError ServeError Error del servidor
+ *
+ * @apiErrorExample ServeError: 500
+ *     HTTP/1.1 500 Internal Server Error
+ *     {
+           "ok": false,
+           "message": "Mensaje de error del servidor",
+ *     }
+ *
+ */
 router.route('/')
     .get(role_controller_1.getRoles)
     .post(role_controller_1.createRole);
--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -3,173 +3,37 @@ import { searchCategoryByStoreId, searchCategory, getCategories, createCategory,
 
 const router = Router();
 
-/**
- * @api {get} url/category/ Get categories
- * @apiName GetCategories
- * @apiGroup Category
- * 
- * @apiHeaderExample {json} Header-Example:
- *    {
- *       "flutter_key": "xxxxx",
- *       "token": "xxxx.xxxx.xxxx"
- *    }
- *
- * @apiSuccess {Boolean}  ok                         Success of the petition.
- * @apiSuccess {String}   message                    Message of the petition.
- * @apiSuccess {Object}   result                     Show all categories.  
- * @apiSuccess {String}   result.category_id         The category id.
- * @apiSuccess {String}   result.category_name       The category name.
- *
- * @apiSuccessExample Success-Response:
- *     HTTP/1.1 200 OK
- *     {
-            "ok": true,
-            "message": "GET successful: category",
-            "result": [
-                {
-                    "category_id": 0,
-                    "category_name": "xxxxxx"
-                }
-            ]
- *      }
- *
- * @apiError GetQueryError GET query error
- *
- * @apiErrorExample GetQueryError: 400
- *     HTTP/1.1 400 Not Found
- *     {
-            "ok": false,
-            "message": "GET error"
- *     }
- *
- * @apiErrorExample UpdateApp: 406
- *     HTTP/1.1 406 Need to update
- *     {
-            "ok": false,
-            "message": "Update the app",
- *     }
- *
- * 
- * @apiError ServeError Internal Server Error
- *
- * @apiErrorExample ServeError: 500
- *     HTTP/1.1 500 Internal Server Error
- *     {
-            "ok": false,
-            "message": "Server's message",
- *     }
- * 
- */
-
- /**
- * @api {post} url/category/ Create category
- * @apiName PostCreateCategory
- * @apiGroup Category
- * 
- * @apiHeaderExample {json} Header-Example:
- *    {
- *       "flutter_key": "xxxxx",
- *       "token": "xxxx.xxxx.xxxx"
- *    }
- *
- * @apiSuccess {Boolean}  ok                         Success of the petition.
- * @apiSuccess {String}   message                    Message of the petition.
- *
- * @apiSuccessExample Success-Response:
- *     HTTP/1.1 200 OK
- *     {
-            "ok": true,
-            "message": "INSERT successful: category",
- *     }
- *
- *
- * 
- * @apiError InsertQueryError POST query error
- * @apiErrorExample InsertQueryError: 400
- *     HTTP/1.1 400 Query error
- *     {
-            "ok": false,
-            "message": "INSERT error"
- *     }
- *
- * @apiError Token error
- * @apiErrorExample TokenError: 401
- *     HTTP/1.1 401 Token error
- *     {
-            "ok": false,
-            "message": "Token error"
- *     }
- *
- *
- * @apiErrorExample UpdateApp: 406
- *     HTTP/1.1 406 Need to update
- *     {
-            "ok": false,
-            "message": "Update the app",
- *     }
- *
- * 
- * @apiError ServeError Internal Server Error
- *
- * @apiErrorExample ServeError: 500
- *     HTTP/1.1 500 Internal Server Error
- *     {
-            "ok": false,
-            "message": "Server's message",
- *     }
- * 
- *  
- */
 router.route('/')
     .get(getCategories)
     .post(createCategory);
 
 
 
-
- /**
- * @api {get} url/category/ Get category
- * @apiName GetCategoryBy
- * @apiGroup Category
- * 
- *
- * @apiSuccess {Boolean}  ok                         Success of the petition.
- * @apiSuccess {String}   message                    Message of the petition.
- * @apiSuccess {Object}   result                     Show all categories.  
- * @apiSuccess {String}   result.category_id         The category id.
- * @apiSuccess {String}   result.category_name       The category name.
- *
- * @apiSuccessExample Success-Response:
- *     HTTP/1.1 200 OK
- *     {
-            "ok": true,
-            "message": "GET BY category_id successful: category",
-            "result": [
-                {
-                    "category_id": 0,
-                    "category_name": "xxxxx"
-                }
-            ]
- *     }
- *
- * 
- */
-
 router.route('/:category_id')
     .get(getCategory)
     .put(updateCategory)
     .delete(deleteCategory);
 
+
+
 router.route('/search')
     .post(searchCategory);
+
+
 
 router.route('/search/:store_id')
     .post(searchCategoryByStoreId);
 
+
+
 router.route('/order_by_categoryid/:category_id')
     .get(getCategoriesById);
+
+
 
 router.route('/store/:store_id')
     .get(getCategoriesByStores);
 
+
+    
 export default router;
